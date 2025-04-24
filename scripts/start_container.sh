6e0fdea1b92e62f2ab11f3bc8269991b1cb3da08
Vakram <vasimvasim6303@gmail.com>
2025-04-24 15:53:07 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
-
+docker pull rahul407/simple-python-flask-app16
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 rahul407/simple-python-flask-app16
 
